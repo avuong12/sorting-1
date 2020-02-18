a28e52364d81777175de5a909ed0f35dd2d5e053
Angela Vuong <vuong.angela@gmail.com>
2020-02-18 15:47:14 -0500

    completed merge function

--- mergesort.js
+++ mergesort.js
@@ -2,24 +2,40 @@ function split(wholeArray) {
   let firstHalf;
   let secondHalf;
 
-  const splitLength = Math.floor(wholeArray.length / 2)
+  const splitLength = Math.floor(wholeArray.length / 2);
 
   if (wholeArray.length <= 1) {
     return wholeArray;
   }
 
-  firstHalf = wholeArray.slice(0, splitLength)
-  secondHalf = wholeArray.slice(splitLength)
+  firstHalf = wholeArray.slice(0, splitLength);
+  secondHalf = wholeArray.slice(splitLength);
 
   return [firstHalf, secondHalf];
 }
 
-function merge(array) {
+function merge(array1, array2) {
+  let resultArray = [];
+  let indexArray1 = 0;
+  let indexArray2 = 0;
 
+  while (indexArray1 < array1.length && indexArray2 < array2.length) {
+    if (array1[indexArray1] < array2[indexArray2]) {
+      resultArray.push(array1[indexArray1]);
+      indexArray1++;
+    } else {
+      resultArray.push(array2[indexArray2]);
+      indexArray2++;
+    }
+  }
+
+  if (indexArray2 < array2.length) {
+    return resultArray.concat(array2.slice(indexArray2));
+  } else {
+    return resultArray.concat(array1.slice(indexArray1));
+  }
 }
 
 function mergeSort(array) {
-
   /* your code here */
-
 }
--- mergesort.spec.js
+++ mergesort.spec.js
@@ -1,26 +1,36 @@
 describe('Split Array function', function() {
   it('is able to split an array into two halves', function() {
-    expect(split([1,2,3,4,5,6])).toEqual([[1,2,3],[4,5,6]])
+    expect(split([1, 2, 3, 4, 5, 6])).toEqual([
+      [1, 2, 3],
+      [4, 5, 6],
+    ]);
   });
 
   it('returns original array if array length is less than or equal to 1', function() {
-    expect(split([1])).toEqual([1])
+    expect(split([1])).toEqual([1]);
   });
-
-  
 });
 
-
-
-describe('Merge function', function(){
-  it('is able to merge two sorted arrays into one sorted array', function(){
+describe('Merge function', function() {
+  it('is able to merge two sorted arrays into one sorted array', function() {
     // test the merging algorithm
+    expect(merge([1, 3, 5], [2, 4, 6])).toEqual([1, 2, 3, 4, 5, 6]);
+    expect(merge([1, 3, 5, 7, 9], [2, 4, 6, 8])).toEqual([
+      1,
+      2,
+      3,
+      4,
+      5,
+      6,
+      7,
+      8,
+      9,
+    ]);
   });
 });
 
-
-describe('Merge Sort function', function(){
-  it('is able to merge sort a given array', function(){
+describe('Merge Sort function', function() {
+  it('is able to merge sort a given array', function() {
     // test the merging algorithm
   });
 });
